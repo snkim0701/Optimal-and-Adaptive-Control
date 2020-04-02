537d40f29355b963780bf75eb70399a6dd7f70ae
S.Kim <snkim0701@daum.net>
2020-04-03 08:00:20 +0900

    Deterministic LQR

--- Week_7.m
+++ Week_7.m
@@ -1,6 +1,6 @@
 
 
-%% linear homegeneous
+%% linear homegeneous - differential equation-ode
 clear all; clc; clf
 tspan = [ 0 5]; % domain
 x0 = 1;         % initial point
@@ -30,7 +30,7 @@ x0 = 2;         % initial point
 [t,x] = ode45(@(t,x) -x + x^2 +1, tspan,x0); % ode45 function.
 plot(t,x); grid on
 
-%% (3-142)  Riccati equation
+%% (3-142) Fig 3.7  Riccati equation
 
 clear all; clc; clf
 tspan = [ 0 1]; % domian
@@ -53,127 +53,72 @@ hold off
 title('Fig.3.7 p(t) for various values of rho and phi')
 disp('I am Fine')
 
-%%
-tt = 0:0.001:1;
-Stt = subs(S,t,tt);
-plot(Stt)
 
+%% Example 3.7 : scalar algebraic Riccati equation
+syms  p a rho k
 
-%%
+S =solve(-2*a*p+1-k^2/rho *p^2 ==0,p);
+pretty(S)
+disp('Bing go!')
 
-P = dsolve(diff(x) == -2*a*x - x^2 +1, x(0) == x0);
-pretty(P)
+%% Figure 3.8
+clear all; clc;clf
+a = 4.6;
+k = 0.787;
+for i=1:1000
+    rho = i/10000;
+    y1(i) = -(1/2)*(sqrt(a^2 + 2*k/sqrt(rho)) + sqrt(a^2 -2*k/sqrt(rho)));
+    plot(real(y1), imag(y1),'Linewidth',2); hold on; grid on
+    y2(i) = -(1/2)*(sqrt(a^2 + 2*k/sqrt(rho)) - sqrt(a^2 -2*k/sqrt(rho)));
+    plot(real(y2), imag(y2),'Linewidth',2); 
+end
 
-
-
-%% Week_5 transfer function
-% unknown parameters
-% open loop T.R.
-
-clear all; clc
-syms s a b k
-A =[ 0 1; -a -b];
-B =[0 1]';
-%C = [ 1 0];
-%C = [ 1 0];
-C = [ 0 1];
-I = eye(2,2);
-TR_open = C*inv(s*I-A)*B
-
-% close loop
-FK = [k 0];
-TR_closed = C*inv(s*I-(A-B*FK))*B
-
-%% Week_5 
-a12 = 1;
-a22= -1;
-a34= 1;
-a41 = -11.65;
-a43 = -a41;
-b12=1;
-A = [ 0 a12 0 0; 0 a22 0 0; 0 0 0 a34; a41 0 a43 0];
-B = [0 b12 0 0]';
-eig(A)
-
-
-%% transfer function symboli math
-clear all;clc
-syms a b s
-A =[ 0 1; -a -b];
-B = [0 ; 1];
-u = [ 0 1];
-C= [ 1  0];
-I = eye(2,2);
-Kim=C*inv(s*I - (A - B*u))*B;
-simplify(Kim)
-%% page 49
-clear all;clc
-syms F M g L s k
-A = [ 0 1 0 0; 0 -F/M 0 0; 0 0 0 1; -g/L 0 g/L 0];
-B = [0; 1/M; 0; 0]
-K= [-1 0 1 0];
-C= [ -1 0 1 0];
-I = eye(4,4);
-Kim = (C*inv(s*I - (A-B*K))*B)  % transfer function 
-                                % the ordert of tranfer function should be "4" 
-simplify(Kim)
-%% example 3.1 page 195
-clear all; clc
-syms F M g L s k1 k2 k3 k4
-A = [ 0 1 0 0; 0 -F/M 0 0; 0 0 0 1; -g/L 0 g/L 0];
-B = [0; 1/M; 0; 0];
-K = [ k1 k2 k3 k4];
-I = eye(4,4);
-Den=det(s*I - (A-B*K)) % the charateristic eqn
-pretty(Den)
-%%
-DenM = subs(Den,M,1);
-DenF = subs(DenM,F,1);
-DenL = subs(DenM,L,0.842);
-gs = 11.65*0.842;
-Deng = subs(DenL,g,gs);
-pretty(Deng);
-
-%%  .... I did not get good method to find (3-13). Try to find!
-
-clear all; clc
-syms F M g L s 
-A = [ 0 1 0 0; 0 -F/M 0 0; 0 0 0 1; -g/L 0 g/L 0];
-B = [0; 1/M; 0; 0];
-%K = [ k1 k2 k3 k4];
-K =[65.65 11.00 -72.60,-21.27]; % textbook page 196.(3-13)
-I = eye(4,4);
-Den=det(s*I - (A-B*K)) % the charateristic eqn
-DenM = subs(Den,M,1);
-DenF = subs(DenM,F,1);
-DenL = subs(DenF,L,0.842);
-gs = 11.65*0.842;
-Deng = subs(DenL,g,gs)
-
-%% to get (3-13) 
-clear all;clc
-syms s k1 k2 k3 k4
-a12 = 1;
-a22= -1;
-a34= 1;
-a41 = -11.65;
-a43 = -a41;
-b12=1;
-A = [ 0 a12 0 0; 0 a22 0 0; 0 0 0 a34; a41 0 a43 0];
-B = [0 b12 0 0]';
-eig(A)
-I = eye(4,4);
-K = [k1 k2 k3 k4];
-%K =-[65.65 11.00 -72.60,-21.27]; % textbook page 196.(3-13)
-det(s*I-(A+B*K))
 %
-k2 =-11;
-k1 = -54-233/20
-k4=(108-233/20*k2+233/20)*20/233
-k3=(81-233/20*k1)*20/233
-K=[k1 k2 k3 k4]
+mrho = [0.0055 0.0002 ];
+for i = 1:2
+    M_1(i) = -(1/2)*(sqrt(a^2 + 2*k/sqrt(mrho(i))) + sqrt(a^2 -2*k/sqrt(mrho(i))));
+    plot(real(M_1), imag(M_1),'o','Markersize',10); hold on
+    M_2(i) = -(1/2)*(sqrt(a^2 + 2*k/sqrt(mrho(i))) - sqrt(a^2 -2*k/sqrt(mrho(i))));
+    plot(real(M_2), imag(M_2),'o', 'Markersize',10);
+end
+title('Loci of closed loop roots as a function of rho')
 
-% check the result
-simplify(det(s*I-(A+B*K)))
-eig(A+B*K)  
-display('Bing go !!')
+
+
+%% Figure 3.9 state -space - known parameter values
+
+clear all; clc;clf
+
+% First open loop system model
+a = 4.6;
+k = 0.787;
+rho = 0.0002;
+A =[0 1; 0 -a];
+B = [0 k]';
+C = [ 1 0];
+Q =C'*C;
+R = rho;
+
+% find the optimal controller
+sys = ss(A,B,C,[ ]);
+[K,P,E] = lqr(sys,Q,R)
+
+% The closed loop system
+AC =A -B*B'*(1/rho)*P;
+sysClosed =ss(AC,B,C,[ ]);
+x0 =[0.1; 0];
+
+[y,T,x] =initial(sysClosed,x0); grid on
+subplot(2,1,1)
+plot(T,y); grid on
+title('the output angle with rho = 0.0002,with initial point')
+
+u = -B'*P*(1/(rho)*x');
+subplot(2,1,2)
+plot(T,u); grid on
+title('the control input of DC motor voltage')
+
+
+%%
+
+
